1686ae39dfeb14f37a2cfda04ee8b0df6473081c
ADHARSH B <adharshb@ADHARSHs-MBP.domain.name>
2022-12-16 11:28:20 +0530

    new-account-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "adharshdevoloper+19@gmail.com"
+    AccountName               = "sandbox-aft-test"
+    ManagedOrganizationalUnit = "Aft"
+    SSOUserEmail              = "adharshdevoloper@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
